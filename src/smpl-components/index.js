88aed614d0ad57f309d20dafd1d7edf03be44710
nikita-zotcik <nikita.zotcik11@gmail.com>
2018-07-06 20:39:56 +0300

    NEWS

--- src/smpl-components/index.js
+++
@@ -1,9 +0,0 @@
-import RadioButton from './radioButton/radioButton';
-import ModalVideo from './modals/modalVideo';
-import Checkbox from './checkbox/checkbox';
-import Spinner from './spinner/spinner';
-import Button from './button/button';
-import Select from './select/select';
-import Input from './input/input';
-
-export { Button, Select, Input, RadioButton, Checkbox, ModalVideo, Spinner };
