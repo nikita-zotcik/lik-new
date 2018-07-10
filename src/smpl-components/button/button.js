98268f734e9e32669c013fe9061e408f3330e0f4
nikita-zotcik <nikita.zotcik11@gmail.com>
2018-07-10 11:44:22 +0300

    something

--- src/smpl-components/button/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ title = 'Default', className = 'button', onChange, styles = {}, ...props }) => {
-    return (
-        <button className={className} onClick={onChange} style={styles} {...props}>{title}</button>
-    )
-};
-
-export default Button;
